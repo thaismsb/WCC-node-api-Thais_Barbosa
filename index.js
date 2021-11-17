957e50e28a59df6b3c83883f78fae5573e32cdca
Thais Barbosa <thaismsbrasil@hotmail.com>
2021-11-16 21:39:47 -0300

    rotas wcc

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ const express = require("express");
 
 const app = express();
 
-const port = 8080;
+const port = 8000;
+
+app.use(express.json());
 
 app.get("/", function(req,res){
     res.send("Minha primeira requisição")
-})
+});
 app.get("/segunda-req", function(req,res){
     res.send("Minha SEGUNDA requisição")
-})
+});
 app.get("/com-parametros", function(req,res){
-    res.send("Com parâmetros funciona! Sabadou " +  req.query.nome)
+    if(req.query.nome === "Thaís"){
+        res.send("Thaís chamou uma requisição!")
+    }
+    res.send("Com parâmetros funciona! Sabadou " +  req.query.nome + " " + req.query.sobrenome);
+});
+
+app.post("/meu-primeiro-post",function(req,res){
+res.send("Meu post funciona");
+});
+
+app.put("/meu-primeiro-put/:id",function(req,res){
+    console.log(req.body, req.params.id)
+res.send("Meu put funciona");
+});
+
+app.delete("/meu-primeiro-delete/:id",function(req,res){
+    console.log(req.params.id);
+    res.send("Meu delete funciona " + req.params.id);
 })
 
 app.listen(port,function(){
